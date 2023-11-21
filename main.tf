b5415174e993e1e0c2a95396a11f6e1adc6166c6
Samaysinghai <88703605+Samaysinghai@users.noreply.github.com>
2023-11-21 15:58:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,53 @@
-provider "github" {
-  owner = "Samaysinghai"
-  token = ""
+// changed resource to data to use existing rg
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
 }
-
-data "github_repository" "current" {
-  full_name = "https://github.com/Rahulsurya1/checkaccesstoken.git"
+ 
+data "azurerm_subscription" "current" {
 }
-
-output "repository_url" {
-  value = data.github_repository.current.html_url
+ 
+data "azurerm_user_assigned_identity" "msi" {
+  name                = var.managed_identity_name
+  resource_group_name = var.managed_identity_rg
 }
-
-output "repository_ssh_url" {
-  value = data.github_repository.current.ssh_url
+ 
+resource "random_string" "test_id" {
+  length    = 6
+  min_lower = 6
+}
+ 
+ 
+module "aci" {
+  source                  = "git::https://github.com/Rahulsurya1/checkaccesstoken.git
+  aci_name                = var.aci_name
+  resource_group          = data.azurerm_resource_group.rg
+  aci_subnet_ids          = var.aci_subnets
+  managed_identity        = data.azurerm_user_assigned_identity.msi
+  use_registry_credential = true
+  tags                    = var.tags
+  enable_debug            = false
+ 
+  enable_azure_monitoring_metrics = true
+  azure_metrics_exporter_prometheus_jobs = templatefile("manifests/jobs.yaml", {
+    subscription_id = data.azurerm_subscription.current.subscription_id
+  })
+ 
+  # fluentd parameters:
+  # fluentd_enrich_and_filter_config = templatefile("manifests/fluentd.conf", {})
+  enable_diagnostic_event_hub = true
+  diag_event_hub = {
+    subnet_id = var.eventhub_subnet_id
+    # Only needed to ensure unicity of cicd automated testings
+    name_suffix = random_string.test_id.result
+  }
+ 
+  dt_config = {
+    env             = var.dynatrace_env
+    ingestion_token = var.dynatrace_token
+    metrics_dimensions = {
+      z_env      = var.environment
+      z_team     = var.tags.team
+      z_activity = var.tags.activity
+    }
+  }
 }
