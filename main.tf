a2f3d8c31f8408a328164aa5fc8aac50d02f1840
Samaysinghai <88703605+Samaysinghai@users.noreply.github.com>
2023-11-22 17:01:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,20 @@
-
 provider "github" {
-  # The GitHub provider requires authentication via a token
-  owner=Rahulsurya1
-  token=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
-}
-
-
+  owner = var.gh_username
+  token = var.gh_token
+} 
 variable "repository_url" {
   description = "https://github.com/Rahulsurya1/checkaccesstoken.git"
 }
+
+output "repository_url" {
+  value = data.github_repository.current.html_url
+}
+
+output "repository_ssh_url" {
+  value = data.github_repository.current.ssh_url
+}
+
+
+
 
 
