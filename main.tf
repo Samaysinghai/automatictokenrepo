499329e45a064c97faf073de5f72aadb5493b59d
Samaysinghai <88703605+Samaysinghai@users.noreply.github.com>
2023-11-25 18:10:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 module "aci" {
   source = "git::https://github.com/Samaysinghai/newone.git"
 }
+terraform {
+  backend "remote" {
+    organization = "Samaysinghai"
+    workspaces = {
+      name = "Samaysinghai/newone"
+    }
+  }
+}
