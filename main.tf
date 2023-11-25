2dc1d8fc055e68872afaa8c6a05050f2fad27085
Samaysinghai <88703605+Samaysinghai@users.noreply.github.com>
2023-11-25 15:50:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 module "aci" {
-  source = "git::https://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@github.com/Rahulsurya1/checkaccesstoken.git"
+  source = "git::https://Rahulsurya1/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@github.com/Rahulsurya1/checkaccesstoken.git"
   
 }
