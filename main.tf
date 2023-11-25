d1db5a9622d75a916db2845fe608e793ae5bcbea
Samaysinghai <88703605+Samaysinghai@users.noreply.github.com>
2023-11-25 16:17:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 module "aci" {
-  source = "git::https://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@github.com/Samaysinghai/newone.git"
+  source = "git::https://Samaysinghai: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@github.com/Samaysinghai/newone.git"
 }
