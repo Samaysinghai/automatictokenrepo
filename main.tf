93539c181e029689deab7ad02d05017c3a077cb2
Samaysinghai <88703605+Samaysinghai@users.noreply.github.com>
2023-11-25 19:27:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,23 @@
 module "aci" {
   source = "git::https://github.com/Samaysinghai/newone.git"
+
+ github_credentials = {
+    username = "Samaysinghai"
+    token    = "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"
+  }
 }
+
+
+variable "github_credentials" {
+  description = "GitHub credentials for authentication"
+  type        = map(string)
+}
+
+provider "github" {
+  owner = var.github_credentials["Samaysinghai"]
+  token = var.github_credentials["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"]
+}
+
+
+
 
