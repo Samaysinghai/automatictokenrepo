0b44fcaae552b2ede0e89cbe94ca71670a337620
Samaysinghai <88703605+Samaysinghai@users.noreply.github.com>
2023-11-25 15:47:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,4 @@
 module "aci" {
-  source = "git::https://github.com/Rahulsurya1/checkaccesstoken.git"
-
-  // Additional configuration options for the module...
-
-  // Authentication information
-  pat_token     = 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
-  git_username  = Rahulsurya1
+  source = "git::https://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@github.com/Rahulsurya1/checkaccesstoken.git"
   
 }
