81b00944906625f6d264cf5f6c119c3ddc1050e4
Samaysinghai <88703605+Samaysinghai@users.noreply.github.com>
2023-11-25 15:42:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,10 @@
-provider "github" {
-  owner = var.gh_username
-  token = var.gh_token
-} 
-variable "repository_url" {
-  description = "https://github.com/Rahulsurya1/checkaccesstoken.git"
+module "aci" {
+  source = "git::https://github.com/Rahulsurya1/checkaccesstoken.git"
+
+  // Additional configuration options for the module...
+
+  // Authentication information
+  pat_token     = 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
+  git_username  = Rahulsurya1
+  
 }
-
-output "repository_url" {
-  value = data.github_repository.current.html_url
-}
-
-output "repository_ssh_url" {
-  value = data.github_repository.current.ssh_url
-}
-
-
-
-
-
