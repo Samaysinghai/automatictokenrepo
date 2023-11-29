484526e12be3768850297b806a372575d9659e60
Samaysinghai <88703605+Samaysinghai@users.noreply.github.com>
2023-11-29 12:56:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "aci" {
-  source = "https://github.com/Samaysinghai/newone.git"
+  source = "git::https://github.com/Samaysinghai/newone.git"
   user_name = "Samaysinghai"
   token = "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"
 }
