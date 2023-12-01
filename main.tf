c74266e077924a41b5d4c3aded89b8384651dba0
Samaysinghai <88703605+Samaysinghai@users.noreply.github.com>
2023-12-01 12:46:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,7 @@
-provider "github" {
-  owner  = "Samaysinghai"
-  token  = var.token
-}
 
 module "aci" {
-  source = "git::https://github.com/Samaysinghai/newone.git"
-  user_name = "Samaysinghai"
-  token = "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"
+  source = "git::git@github.com:Samaysinghai/automatictokenrepo.git"
+  
 }
 
 
